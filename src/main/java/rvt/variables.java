bf17552178a9ffa9234f9500b3a437c4d2652fa8
24DP2KCere <A240475KC@rvt.lv>
2025-09-10 13:07:46 +0300

    variables

---
+++ src/main/java/rvt/variables.java
@@ -0,0 +1,22 @@
+package rvt;
+
+public class variables {
+    public static void main(String[] args) {
+      String text = ("Zetor");
+      int sv = (9000);
+      double nvs = (0.1);
+
+      System.out.println("Chicken:");
+      System.out.println(sv);
+      System.out.println("Bacon:");
+      System.out.println(nvs); 
+      System.out.println("Tractor:");
+      System.out.println(text);
+      System.out.println("And finally, a summary:");
+      System.out.println(sv);
+      System.out.println(nvs);
+      System.out.println(text);
+
+
+    }
+}
